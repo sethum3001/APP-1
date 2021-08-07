e7d3c09462c3024109bd1def01d32d66e95784d2
Sethum3001 <sethumrashmika3001@gmail.com>
2021-08-07 21:34:45 +0530

    2

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,14 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.id.Configurable;
+
 public class Demo {
     public static void main(String[] args) {
-
+        Student student = new Student("S001","Nimal","Colombo","011");
+        Configuration configuration= new Configuration();
+        SessionFactory sessionFactory=configuration.buildSessionFactory();
+        Session session= sessionFactory.openSession();
+        session .save(student);
     }
 }
